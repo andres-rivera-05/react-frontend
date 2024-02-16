0d3b4c79daa7e04b4a532f693525b87d8091b0bb
andres <148400031+andres-rivera-05@users.noreply.github.com>
2024-02-16 17:03:25 -0600

    Completando Page inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import { Home  } from './components/Home'
+import { Reservas } from './components/Reservas'
+import { Categoris } from './components/Categoris'
+import { Usuarios } from './components/Usuarios'
+import { NavBar } from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
+     <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/reservas' element={<Reservas/>}></Route>
+          <Route path='/categorias' element={<Categoris/>}></Route>
+          <Route path='/usuarios' element={<Usuarios/>}></Route>
+        </Routes>
+     </BrowserRouter>
+
     </>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,84 @@
 import React from 'react'
+import { NavBar } from './NavBar'
 
 export const Home = () => {
+
+    
+const TablaLibros =[
+
+    {
+        position : 1,
+        equipo: "Real Madrid",
+        estado : "Disponible"
+    },
+    {
+        position : 2,
+        equipo : "FC Barcelona",
+        estado : "Reservado"
+    },
+    {
+        position : 3,
+        equipo : "Atletico Madrid",
+        estado : "Disponible"
+    },
+    {
+        position : 4,
+        equipo : "Real Sociedad",
+        estado : "Disponible"
+    },
+    {
+        position : 5,
+        equipo: "Real Madrid",
+        estado : "Disponible"
+    },
+    {
+        position : 6,
+        equipo : "FC Barcelona",
+        estado : "Reservado"
+    },
+    {
+        position : 7,
+        equipo : "Atletico Madrid",
+        estado : "Reservado"
+    },
+    {
+        position : 8,
+        equipo : "Real Sociedad",
+        estado : "Reservado"
+    }
+]
   return (
     <>
     
-    <section></section>
+    <div className='container col-8 mt-5'>
+            <h1 className='text-center text-white'>Sistema Biblioteca</h1>
+            <div className='mt-5'>
+               <NavBar></NavBar>
+            </div>
+        
+            <table className='table table-bordered border-secondary'>
+                <thead>
+                    <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Libros</th>
+                    <th scope="col">Autor</th>
+                    <th scope="col">Estado</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { TablaLibros.map( (item) =>(
+                         
+                         <tr key={item.position}>
+                            <td>{item.position}</td>
+                            <td>{item.equipo}</td>
+                            <td>NAN</td>
+                            <td>{item.estado}</td>
+                       </tr>
+
+                    ))}  
+                </tbody>
+            </table>
+        </div>
 
     
     </>
